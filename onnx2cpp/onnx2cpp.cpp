1532aa1b358b638c815cfaa56a9266c04f41062b
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-04-17 16:22:54 +0200

    Initial commit

---
+++ onnx2cpp/onnx2cpp.cpp
@@ -0,0 +1,12 @@
+﻿// onnx2cpp.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "onnx2cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ onnx2cpp/onnx2cpp.h
@@ -0,0 +1,9 @@
+﻿// onnx2cpp.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+#include "onnx.pb.h"
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
