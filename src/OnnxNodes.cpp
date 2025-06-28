3f2734a59d3c7e6200ecc447df57cecb04fdb61e
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-06-28 14:32:38 +0200

    Introduce cpp files for operators and system which allows to register theses classes

--- src/OnnxNodes.cpp
+++ src/OnnxNodes.cpp
@@ -4,6 +4,9 @@
 #include <variant>
 #include <type_traits>
 
+
+
+
 OnnxNode::OnnxNode(onnx::NodeProto nodeProto)
 {
 	for (std::string val : nodeProto.input())
@@ -59,9 +62,11 @@ OnnxNode::OnnxNode(onnx::NodeProto nodeProto)
 			attributes[att.name()] = std::vector<onnx::TypeProto>(att.type_protos().begin(), att.type_protos().end());
 			break;
 		case onnx::AttributeProto_AttributeType_UNDEFINED:
+			std::cout << "Warning: Undefined attribute type for " << att.name() << std::endl;
 			// Do nothing, undefined type
 			break;
 		default:
+			std::cout << "Warning: Unknown attribute type for " << att.name() << att.type() << std::endl;
 			break;
 		}
 			}
@@ -73,37 +78,33 @@ std::string OnnxNode::GetName() const{
 	return name;
 }
 
-std::string OnnxNode::GetVarInitString() const {
-	std::string res = op_type + "(";
-	res += join(input, ", ");
-	if (input.size() > 0 && output.size() > 0)
-		res += ", ";
-	res += join(output, ", ");
-	if (attributes.size() > 0)
-		res += ", ";
+std::string OnnxNode::GetParamsString() const {
+	std::string res = "";
 	if (attributes.size() > 0) {
+		int counter = 1;
 		res += op_type + "Params{";
-		for (auto& [key, value] : attributes) {
+		for (auto it = attributes.begin(); it != attributes.end(); ++it) {
+			auto& [key, value] = *it;
 			if (value.type() == typeid(float)) {
-				res += "." + key + "= " + std::to_string(std::any_cast<float>(value)) + ", ";
+				res += "." + key + "= " + std::to_string(std::any_cast<float>(value)) + "";
 			}
 			else if (value.type() == typeid(int64_t)) {
-				res += "." + key + "= " + std::to_string(std::any_cast<int64_t>(value)) + ", ";
+				res += "." + key + "= " + std::to_string(std::any_cast<int64_t>(value)) + "";
 			}
 			else if (value.type() == typeid(std::string)) {
-				res += "." + key + "= \"" + std::any_cast<std::string>(value) + "\", ";
+				res += "." + key + "= \"" + std::any_cast<std::string>(value) + "\"";
 			}
 			else if (value.type() == typeid(onnx::TensorProto)) {
-				res += "." + key + "= " + std::any_cast<onnx::TensorProto>(value).name() + ", ";
+				res += "." + key + "= " + std::any_cast<onnx::TensorProto>(value).name() + "";
 			}
 			/*else if (value.type() == typeid(std::vector<float>)) {
-				res += "." + key + ": [" + join(std::any_cast<std::vector<float>>(value), ", ") + "], ";
+				res += "." + key + ": [" + join(std::any_cast<std::vector<float>>(value), ", ") + "]";
 			}
 			else if (value.type() == typeid(std::vector<int64_t>)) {
-				res += "." + key + ": [" + join(std::any_cast<std::vector<int64_t>>(value), ", ") + "], ";
+				res += "." + key + ": [" + join(std::any_cast<std::vector<int64_t>>(value), ", ") + "]";
 			}
 			else if (value.type() == typeid(std::vector<std::string>)) {
-				res += "." + key + ": [" + join(std::any_cast<std::vector<std::string>>(value), ", ") + "], ";
+				res += "." + key + ": [" + join(std::any_cast<std::vector<std::string>>(value), ", ") + "]";
 			}
 			else if (value.type() == typeid(std::vector<onnx::TensorProto>)) {
 				res += "." + key + ": [";
@@ -111,24 +112,48 @@ std::string OnnxNode::GetVarInitString() const {
 				for (const auto& tensor : tensors) {
 					res += tensor.name() + ", ";
 				}
-				res += "], ";
+				res += "]";
 			}*/
 			else if (value.type() == typeid(onnx::GraphProto)) {
-				res += "." + key + "= Graph, "; // Platzhalter für Graph-Darstellung
+				res += "." + key + "= Graph"; // Platzhalter für Graph-Darstellung
 			}
 			else if (value.type() == typeid(onnx::TypeProto)) {
-				res += "." + key + "= TypeProto, "; // Platzhalter für TypeProto-Darstellung
+				res += "." + key + "= TypeProto"; // Platzhalter für TypeProto-Darstellung
 			}
 			else {
-				res += "." + key + "= UnknownType, ";
+				res += "." + key + "= UnknownType";
 			}
+			if (counter < attributes.size()) res += ", ";
+			counter += 1;
 		}
-		res.erase(res.size() - 2);
+
 		res += "}";
+		
 	}
-	res += "); // " + name;
 	return res;
 }
+
+std::string OnnxNode::GetVarInitString() const {
+	std::string res = "";
+	std::unique_ptr<OperatorHandler> handler = OperatorHandlerFactory::create(*this);
+	if (handler && handler->OperatorSpecificGeneration()) {
+		res = handler->GetVarInitString();
+	}
+	else {
+		res += op_type + "(";
+		res += join(input, ", ");
+		if (input.size() > 0 && output.size() > 0)
+			res += ", ";
+		res += join(output, ", ");
+		if (attributes.size() > 0) {
+			res += ", " + GetParamsString();
+		}
+		res += "); // " + name;
+	}
+		
+	return res;
+}
+
 
 
 
@@ -145,9 +170,9 @@ void OnnxNodes::Add(const OnnxNode var) {
 	if ((opTypes.end() == std::find(opTypes.begin(), opTypes.end(), opType))) {
 		opTypes.push_back(opType);
 	}
-	else {
-		std::cout <<"var " << var.GetName() << " is already added" << std::endl;
-	}
+	//else {
+	//	std::cout <<"var " << var.GetName() << " is already added" << std::endl;
+	//}
 }
 int OnnxNodes::GetCount() const {
 	return vars.size();
--- src/OnnxNodes.h
+++ src/OnnxNodes.h
@@ -3,6 +3,9 @@
 #include <vector>
 #include <map>
 #include <any>
+#include <memory>
+#include <functional>
+//#include "Operators/Add.h"
 class OnnxNode
 {
 public:
@@ -10,6 +13,10 @@ public:
 	std::string GetName() const;
 	std::string GetOpType() const { return op_type; }
 	std::string GetVarInitString() const;
+	std::string GetParamsString()const;
+	std::vector<std::string> GetInputs() const { return input; }
+	std::vector<std::string> GetOutputs() const { return output; }
+	std::map<std::string, std::any> GetAttributes() const { return attributes; }
 	std::any GetAttribute(const std::string& name) const {
 		auto it = attributes.find(name);
 		if (it != attributes.end()) {
@@ -17,6 +24,7 @@ public:
 		}
 		return std::any();
 	}
+
 private:
 	std::vector<std::string> input;
 	std::vector<std::string> output;
@@ -48,4 +56,48 @@ private:
 	std::vector<std::string> opTypes;
 };
 
+class OperatorHandler {
+public:
+	OperatorHandler(const OnnxNode node) : node(node) {};
+	virtual ~OperatorHandler() = default;
+	virtual bool OperatorSpecificGeneration() const { return false; }
+	virtual std::string GetVarInitString() const { return ""; }
+ protected:
+	const OnnxNode node;
+};
 
+class OperatorHandlerFactory {
+public:
+	using Creator = std::function<std::unique_ptr<OperatorHandler>(OnnxNode)>;
+	using Map = std::map<std::string, Creator>;
+
+	static bool registerHandler(const std::string& name, Creator creator) {
+		std::cout << name << std::endl; 
+		GetMap()[name] = creator;
+		return true;
+	}
+
+	static std::unique_ptr<OperatorHandler> create(const OnnxNode node) {
+		auto& map = GetMap();
+		auto it = map.find(node.GetOpType());
+		if (it != map.end()) {
+			return it->second(node);
+		}
+		return nullptr;
+	}
+
+private:
+	static Map& GetMap() {
+
+		static Map map;
+		return map;
+	}
+};
+
+// Makro für die Registrierung  
+#define REGISTER_OPERATOR_HANDLER(CLASS, NAME) \
+           namespace { \
+               static bool CLASS##_registered = OperatorHandlerFactory::registerHandler(NAME, [](const OnnxNode& node) { \
+                   return std::make_unique<CLASS>(node); \
+               }); \
+           }
