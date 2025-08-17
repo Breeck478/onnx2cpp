53e7cd49ab97b88f0fd9602ed5f5d26628b06f3b
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-08-17 22:23:36 +0200

    Correct constant and ConstantOfShape implementation

--- src/Operators/Constant.cpp
+++ src/Operators/Constant.cpp
@@ -12,22 +12,61 @@ public:
 	bool OperatorNeedsInclude() const override {
 		return false; // This operator does not need an include
 	}
-	void GetOpSpecificNodeGenString(std::ostringstream & stream) const override {
+	void GetOpSpecificNodeGenString(std::ostringstream& stream) const override {
 
 		try {
 			if (node->GetOutputNames().size() > 0 && node->GetInputNames().size() == 0) {
 				for (auto& att : node->GetAttributes())
 				{
-					if (att.first == "value" || att.first == "sparse_value") {
+					if (att.first == "value") {
 						auto value = std::any_cast<onnx::TensorProto>(att.second);
 						OnnxConst constant(value);
 						constant.Name(node->GetOutputNames()[0]);
-						std::string res = constant.GetConstantString(false);
-						stream << res;
+						stream << constant.GetConstantString(false);
 					}
-					else if (att.first.rfind("value_", 0) == 0) {
-						std::cout << att.first << std::endl;
-						stream << att.first + " = ";
+					else if (att.first.starts_with("value_")) { //starts with "value_"
+						if (node->GetOutputs().size() == 1) {
+							OnnxTensor* outputVar = node->GetOutputs()[0];
+							stream << outputVar->Name() << " = { ";
+							if (att.first == "value_ints") {
+								auto value = std::any_cast<std::vector<int64_t>>(att.second);
+								 stream << Join(value, ", ");
+							}
+							else if (att.first == "value_int") {
+								auto value = std::any_cast<int64_t>(att.second);
+								stream << value;
+							}
+							else if (att.first == "value_floats") {
+								auto value = std::any_cast<std::vector<float>>(att.second);
+								stream  << Join(value, ", ");
+							}
+							else if (att.first == "value_float") {
+								auto value = std::any_cast<float>(att.second);
+								stream << value;
+							}
+							else if (att.first == "value_strings") {
+								auto value = std::any_cast<std::vector<std::string>>(att.second);
+								stream << Join(value, ", ");
+							}
+							else if (att.first == "value_string") {
+								auto value = std::any_cast<std::string>(att.second);
+								stream << value;
+							}
+							else {
+								std::cerr << "Unknown value type in Constant operator: " << att.first << std::endl;
+								continue;
+							}
+							stream << outputVar->Name() << " = };\n";
+							stream << outputVar->Name() << " = " << outputVar->Name() << ".reshape({" << Join(outputVar->Shape(), ", ") + "});\n";
+						}
+						else {
+							std::cerr << "Constant operator must have exact one output." << std::endl;
+						}
+					}
+					else if (att.first == "sparse_value")
+					{
+						std::cerr << "Sparse values are not supported in Constant operator: " << node->GetName() << std::endl;
+						continue;
 					}
 					else {
 						std::cerr << "Unknown attribute in Constant operator: " << att.first << std::endl;
--- src/Operators/ConstantOfShape.cpp
+++ src/Operators/ConstantOfShape.cpp
@@ -10,26 +10,36 @@ public:
 	bool OperatorSpecificNodeGeneration() const override {
 		return true; // This operator has specific generation logic
 	}
+	bool OperatorNeedsInclude() const override {
+		return false; // This operator does not need an include
+	}
+	bool OperatorSpecificTensorTypes() const override {
+		return true; // This operator has specific tensor types
+	}
 	void GetOpSpecificNodeGenString(std::ostringstream & stream) const override {
 
 		try {
 			if (node->GetOutputNames().size() == 1 && node->GetInputNames().size() == 1) {
-				for (auto& att : node->GetAttributes())
-				{
-					if (att.first == "value") {
-						auto value = std::any_cast<onnx::TensorProto>(att.second);
+
+					if (node->GetAttribute("value").has_value()) {
+						auto value = std::any_cast<onnx::TensorProto>((node->GetAttribute("value")));
 						OnnxConst constant(value);
-						constant.Name(node->GetOutputNames()[0]);
-						stream << constant.GetConstantString();
-					}
-					else if (att.first.rfind("value_", 0) == 0) {
-						std::cout << att.first << std::endl;
-						stream << att.first + " = ";
+						
+						if (constant.GetDataAsAny().size() != 1)
+							std::runtime_error("ERROR(ConstantOfShapeHandler::GetOpSpecificNodeGenString): ConstantOfShape operator must have exactly one value in the Attribute tensor.");
+
+						auto valString = constant.GetDataAsString(false);
+						valString.erase(0, 5); // Remove "value" (name of this tensor)
+						valString = RemoveChars(valString, "{} =;"); // Remove curly braces and equal
+						// Output shape is simular to shape given by the input
+						stream <<  node->GetOutputNames()[0] << ".resize({" << Join(node->GetOutputs()[0]->Shape(), ",") << "});\n";
+						stream << node->GetOutputNames()[0] << " = " << "xt::full_like(" << node->GetOutputNames()[0] << ", " << valString << ");\n";
 					}
 					else {
-						std::cerr << "Unknown attribute in Constant operator: " << att.first << std::endl;
+						stream <<  node->GetOutputNames()[0] << ".resize({" << Join(node->GetOutputs()[0]->Shape(), ",") << "});\n";
+						stream << node->GetOutputNames()[0] << " = " << "xt::full_like(" << node->GetOutputNames()[0] << ", 0);\n";
 					}
-				}
+				
 			}
 			else {
 				std::cerr << "Constant of Shape operator must have exactly one output and input." << std::endl;
