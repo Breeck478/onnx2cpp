d1f78b4b1cf82b04ba026601b2743225245c2219
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-08-20 01:25:23 +0200

    Fixed ArgMax and ArgMin with negative axis

--- TestInclude/Operators/ArgMax.h
+++ TestInclude/Operators/ArgMax.h
@@ -7,41 +7,38 @@ struct ArgMaxParams
   const int select_last_index = 0;
 };
 template <typename T>
-void ArgMax(
-    const xt::xarray<T> &data,
-    xt::xarray<int64_t> &reduced,
-    const ArgMaxParams &params = ArgMaxParams())
+void ArgMax(const xt::xarray<T>& data, xt::xarray<int64_t>& reduced, const ArgMaxParams& params = ArgMaxParams())
 {
-  int axis = params.axis;
-  int keepdims = params.keepdims;
-  int select_last_index = params.select_last_index;
+    int axis = params.axis < 0 ? (data.dimension() + params.axis) : params.axis;
+    int keepdims = params.keepdims;
+    int select_last_index = params.select_last_index;
 
-  if (select_last_index == 0)
-  {
-    // _argmax
-    xt::xarray<T> tempRes = xt::argmax(data, axis);
-    if ((keepdims == 1) && (tempRes.dimension() < data.dimension()))
+    if (select_last_index == 0)
     {
-      reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        // _argmax
+        xt::xarray<T> tempRes = xt::argmax(data, axis);
+        if ((keepdims == 1) && (tempRes.dimension() < data.dimension()))
+        {
+            reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        }
+        else
+        {
+            reduced = xt::cast<int64_t>(tempRes);
+        }
     }
     else
     {
-      reduced = xt::cast<int64_t>(tempRes);
+        // _argmax_use_numpy_select_last_index
+        xt::xarray<T> tempData = xt::flip(data, axis);
+        xt::xarray<T> tempRes = xt::argmax(tempData, axis);
+        tempRes = tempData.shape()[axis] - tempRes - 1;
+        if (keepdims == 1)
+        {
+            reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        }
+        else
+        {
+            reduced = xt::cast<int64_t>(tempRes);
+        }
     }
-  }
-  else
-  {
-    // _argmax_use_numpy_select_last_index
-    xt::xarray<T> tempData = xt::flip(data, axis);
-    xt::xarray<T> tempRes = xt::argmax(tempData, axis);
-    tempRes = tempData.shape()[axis] - tempRes - 1;
-    if (keepdims == 1)
-    {
-      reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
-    }
-    else
-    {
-      reduced = xt::cast<int64_t>(tempRes);
-    }
-  }
 }
--- TestInclude/Operators/ArgMin.h
+++ TestInclude/Operators/ArgMin.h
@@ -8,41 +8,38 @@ struct ArgMinParams
   const int select_last_index = 0;
 };
 template <typename T>
-void ArgMin(
-    const xt::xarray<T> &data,
-    xt::xarray<int64_t> &reduced,
-    const ArgMinParams &params = ArgMinParams())
+void ArgMin(const xt::xarray<T>& data, xt::xarray<int64_t>& reduced, const ArgMinParams& params = ArgMinParams())
 {
-  int axis = params.axis;
-  int keepdims = params.keepdims;
-  int select_last_index = params.select_last_index;
+    int axis = params.axis < 0 ? (data.dimension() + params.axis) : params.axis;
+    int keepdims = params.keepdims;
+    int select_last_index = params.select_last_index;
 
-  if (select_last_index == 0)
-  {
-    // _argmin
-    xt::xarray<T> tempRes = xt::argmin(data, axis);
-    if ((keepdims == 1) && (tempRes.dimension() < data.dimension()))
+    if (select_last_index == 0)
     {
-      reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        // _argmin
+        xt::xarray<T> tempRes = xt::argmin(data, axis);
+        if ((keepdims == 1) && (tempRes.dimension() < data.dimension()))
+        {
+            reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        }
+        else
+        {
+            reduced = xt::cast<int64_t>(tempRes);
+        }
     }
     else
     {
-      reduced = xt::cast<int64_t>(tempRes);
+        // _argmin_use_numpy_select_last_index
+        xt::xarray<T> tempData = xt::flip(data, axis);
+        xt::xarray<T> tempRes = xt::argmin(tempData, axis);
+        tempRes = tempData.shape()[axis] - tempRes - 1;
+        if (keepdims == 1)
+        {
+            reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
+        }
+        else
+        {
+            reduced = xt::cast<int64_t>(tempRes);
+        }
     }
-  }
-  else
-  {
-    // _argmin_use_numpy_select_last_index
-    xt::xarray<T> tempData = xt::flip(data, axis);
-    xt::xarray<T> tempRes = xt::argmin(tempData, axis);
-    tempRes = tempData.shape()[axis] - tempRes - 1;
-    if (keepdims == 1)
-    {
-      reduced = xt::cast<int64_t>(xt::expand_dims(tempRes, axis));
-    }
-    else
-    {
-      reduced = xt::cast<int64_t>(tempRes);
-    }
-  }
 }
