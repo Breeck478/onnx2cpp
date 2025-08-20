865696ccba7a04602783cedabe1d1fb7f2991e7f
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-08-21 00:08:51 +0200

    Added Softmax Operator

---
+++ TestInclude/Operators/Softmax.h
@@ -0,0 +1,30 @@
+#include <xtensor.hpp>
+struct SoftmaxParams
+{
+	const int axis = -1;
+};
+
+template <typename T>
+void Softmax(const xt::xarray<T>& input, xt::xarray<T>& output, const SoftmaxParams& params = SoftmaxParams()) {
+	int axis = params.axis < 0 ? (input.dimension() + params.axis) : params.axis;
+	typename xt::xarray<T>::shape_type keepdimsShape = {};
+	for (size_t i = 0; i < input.shape().size(); i++) {
+#
+		if (i != axis)
+			keepdimsShape.push_back(input.shape()[i]);
+		else
+			keepdimsShape.push_back(1);
+	};
+	if (input.size() == 0) {
+		output = input;
+		exit;
+	}
+	// manualy reshape tensors to copy keepdims behavior from numpy
+	xt::xarray<T> keepdimsTmp = xt::amax(input, { axis });
+	keepdimsTmp.reshape(keepdimsShape);
+	xt::xarray<T> tmp = input - keepdimsTmp;
+	output = xt::exp(tmp);
+	xt::xarray<T> keepdimsOutput = xt::sum(output, { axis });
+	keepdimsOutput.reshape(keepdimsShape);
+	output = output / keepdimsOutput;
+}
