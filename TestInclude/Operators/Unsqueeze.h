5bcdf24b5a7a7bad089ea9d3bd8133d527040e40
Benjamin Althaus <Benjaminalthaus02@gmail.com>
2025-08-22 01:00:07 +0200

    Cleanup unsqueeze

--- TestInclude/Operators/Unsqueeze.h
+++ TestInclude/Operators/Unsqueeze.h
@@ -12,14 +12,14 @@ void Unsqueeze(const xt::xarray<T> &data, const xt::xarray<int64_t> &axis, xt::x
 {
 	xt::xarray<int64_t> sortedAxis = xt::sort(axis, 1); // axis should be a 1D array of integers
 	expanded = data;
-	int axis_size = expanded.shape().size();
+	int axisSize = expanded.shape().size();
 	for (size_t i = 0; i < sortedAxis.size(); ++i) {
 		int val = sortedAxis[i];
 		if (val < 0) {
-			val = axis_size + val;
+			val = axisSize + val;
 		}
 		expanded = xt::expand_dims(expanded, val);
-		axis_size = expanded.shape().size();
+		axisSize = expanded.shape().size();
 
   }
 }
